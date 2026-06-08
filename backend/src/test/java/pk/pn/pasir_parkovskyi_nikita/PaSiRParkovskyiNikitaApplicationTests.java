19f8fb191b5a732fad230f8a9597d42bfb6479f6
Nikita Parkovskyi <143649497+K1taSun@users.noreply.github.com>
2026-06-08 17:04:44 +0200

    initial commit

---
+++ backend/src/test/java/pk/pn/pasir_parkovskyi_nikita/PaSiRParkovskyiNikitaApplicationTests.java
@@ -0,0 +1,13 @@
+package pk.pn.pasir_parkovskyi_nikita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaSiRParkovskyiNikitaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
